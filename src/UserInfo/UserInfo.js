230a6a428f47df6b8be61a1a985fd8e9c295a0bf
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-07-07 10:33:38 +0800

    add <tbody> label to fix the validateDomNesting warning the browser thrown out.

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -9,22 +9,24 @@ class UserInfo extends Component {
         <h2>{name}</h2>
         <label>Contact Me @</label>
         <table>
-          <tr>
-            <th>Email</th>
-            <td>{email}</td>
-          </tr>
-          <tr>
-            <th>Phone</th>
-            <td>{phone}</td>
-          </tr>
-          <tr>
-            <th>Website</th>
-            <td>{website}</td>
-          </tr>
-          <tr>
-            <th>Company</th>
-            {company ? <td>{company.name}</td> : null}
-          </tr>
+          <tbody>
+            <tr>
+              <th>Email</th>
+              <td>{email}</td>
+            </tr>
+            <tr>
+              <th>Phone</th>
+              <td>{phone}</td>
+            </tr>
+            <tr>
+              <th>Website</th>
+              <td>{website}</td>
+            </tr>
+            <tr>
+              <th>Company</th>
+              {company ? <td>{company.name}</td> : null}
+            </tr>
+          </tbody>
         </table>
       </aside>
     );

e10b810a6d99762c51a3991fc878ac674d96140b
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-07-07 10:28:39 +0800

    show user info at the aside pane.

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -3,7 +3,31 @@ import './UserInfo.scss';
 
 class UserInfo extends Component {
   render() {
-    return <aside className="UserInfo">User Info</aside>;
+    const { name, email, phone, website, company } = this.props.user;
+    return (
+      <aside className="UserInfo">
+        <h2>{name}</h2>
+        <label>Contact Me @</label>
+        <table>
+          <tr>
+            <th>Email</th>
+            <td>{email}</td>
+          </tr>
+          <tr>
+            <th>Phone</th>
+            <td>{phone}</td>
+          </tr>
+          <tr>
+            <th>Website</th>
+            <td>{website}</td>
+          </tr>
+          <tr>
+            <th>Company</th>
+            {company ? <td>{company.name}</td> : null}
+          </tr>
+        </table>
+      </aside>
+    );
   }
 }
 
